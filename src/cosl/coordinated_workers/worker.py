f07ff64677a61cf7d822eb60a721811c1f6fbed7
Pietro Pasotti <starfire.daemon@gmail.com>
2024-07-31 16:28:01 +0200

    fixed tox env but disabled static checks for now

--- src/cosl/coordinated_workers/worker.py
+++ src/cosl/coordinated_workers/worker.py
@@ -163,6 +163,7 @@ class Worker(ops.Object):
                 "options for it to use the Worker."
             )
 
+        "ff".removeprefix("f")
         active_roles: List[str] = [
             role[5:] for role in role_config_options if config[role] is True
         ]
