aa915e1d7b53d4a8f72b098e84527152383bdd2f
Pietro Pasotti <starfire.daemon@gmail.com>
2024-12-10 11:06:33 +0100

    added init files for pyright stubber

---
+++ src/cosl/interfaces/cri_schemas/grafana_datasource_exchange/v0/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical
+# See LICENSE file for licensing details.
+
+"""grafana_datasource_exchange schema.
+
+Keep in sync with charm-relation-interfaces.
+"""
