fa5d8182dcc845b5d8a774f0dc8217d5665bbceb
Michael Thamm <mike.thamm@canonical.com>
2025-10-02 09:19:20 -0400

    chore: Linting

--- tests/test_type_convert_stored.py
+++ tests/test_type_convert_stored.py
@@ -14,15 +14,34 @@ def test_converting_stored_types():
     assert type_convert_stored(StoredDict({}, under={1: 2})) == {1: 2}
 
     # GIVEN nested structures and mixed types
-    assert type_convert_stored(StoredList({}, under=[1, StoredList({}, under=[3, 4])])) == [1, [3, 4]]
-    assert type_convert_stored(StoredDict({}, under={1: StoredList({}, under=[3, 4])})) == {1: [3, 4]}
-    assert type_convert_stored(StoredDict({}, under={1: StoredDict({}, under={2: 3})})) == {1: {2: 3}}
-    assert type_convert_stored(StoredList({}, under=[1, StoredDict({}, under={2: 3})])) == [1, {2: 3}]
+    assert type_convert_stored(StoredList({}, under=[1, StoredList({}, under=[3, 4])])) == [
+        1,
+        [3, 4],
+    ]
+    assert type_convert_stored(StoredDict({}, under={1: StoredList({}, under=[3, 4])})) == {
+        1: [3, 4]
+    }
+    assert type_convert_stored(StoredDict({}, under={1: StoredDict({}, under={2: 3})})) == {
+        1: {2: 3}
+    }
+    assert type_convert_stored(StoredList({}, under=[1, StoredDict({}, under={2: 3})])) == [
+        1,
+        {2: 3},
+    ]
 
     # GIVEN non-standard types
-    assert type_convert_stored(StoredDict({}, under={1: "string", 2: 3.5})) == {1: "string", 2: 3.5}
+    assert type_convert_stored(StoredDict({}, under={1: "string", 2: 3.5})) == {
+        1: "string",
+        2: 3.5,
+    }
     assert type_convert_stored(StoredList({}, under=[None, True, False])) == [None, True, False]
 
     # GIVEN deeply nested structures
-    assert type_convert_stored(StoredDict({}, under={1: StoredList({}, under=[StoredDict({}, under={2: StoredList({}, under=[5])})])})) == {1: [{2: [5]}]}
-
+    assert type_convert_stored(
+        StoredDict(
+            {},
+            under={
+                1: StoredList({}, under=[StoredDict({}, under={2: StoredList({}, under=[5])})])
+            },
+        )
+    ) == {1: [{2: [5]}]}
